44ded5d0fad2fc8b219cbd90dcccb6395188ff84
07anshul <ans2852000@gmail.com>
2021-07-03 14:32:52 +0530

    1

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPR html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title> Happy Birthday Dear Nadya !!!</title>
+
+        <!-- Link CSS Style.css file-->
+
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+        <div class='moon'>
+            <div class='crater1'></div>
+            <div class='crater2'></div>
+            <div class='crater3'></div>
+        </div>
+
+        <canvas id="canvas"></canvas>
+
+        <div id="sea"></div>
+        <div id="beach"></div>
+
+        <img src="https://dl.dropbox.com/s/2k0mtrxc2dqurmh/jumping.png" alt="Jumping-people" id="people" />
+
+        <div id="merrywrap" class="merrywrap">
+            <div class="giftbox">
+                <div class="cover">
+                    <div></div>
+                </div>
+
+                <div class="box"></div>
+            </div>
+
+            <div class="icons">
+                <div class="row">
+                    <span>H</span>
+                    <span>A</span>
+                    <span>P</span>
+                    <span>P</span>
+                    <span>Y</span>
+
+
+                </div>
+                <div class="row">
+                    <span>B</span>
+                    <span>I</span>
+                    <span>R</span>
+                    <span>T</span>
+                    <span>H</span>
+                    <span>D</span>
+                    <span>A</span>
+                    <span>Y</span>
+
+                </div>
+
+                <div class="row">
+                    <span>D</span>
+                    <span>E</span>
+                    <span>A</span>
+                    <span>R</span>
+
+                </div>
+
+
+
+                <div class="row">
+                    <span>N</span>
+                    <span>A</span>
+                    <span>D</span>
+                    <span>Y</span>
+                    <span>A</span>
+
+                </div>
+
+
+
+
+
+                 <div class="column"><h1>"May you receive the greatest of joys and everlasting bliss. You are a gift yourself, and you deserve the best of everything. Happy birthday."</h1>
+
+                </div>
+
+            </div>
+              <img src="disney.png">
+        </div>
+
+
+        <div id="video"></div>
+
+
+        <script src="js/index.js"></script>
+
+    </body>
+</html>
